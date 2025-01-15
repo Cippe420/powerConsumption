672ce3a232c9b0690e2f3e45f087e86e1745477d
Cippe420 <buratti.1863851@studenti.uniroma1.it>
2025-01-15 17:26:07 +0100

    .

--- currentMeasure.py
+++ currentMeasure.py
@@ -8,6 +8,9 @@ def main():
         # Open serial port and read data
         ser = serial.Serial('/dev/ttyUSB0', 9600)
         data = ser.readline().decode('utf-8').rstrip()
+
+        datalist = data.split()
+
         with open('cpuPower.txt','r') as f:
             istante = f.read().strip()
         if istante == 'stop':
@@ -15,7 +18,9 @@ def main():
         else:   
             # Print data
             with open('currentMeasure.csv', 'a') as file:
-                file.write(istante + ', ' + data + '\n')
+                file.write(istante + ', ' + datalist[1] + datalist[3][1::],'\n')
+
+
     return
 
 
