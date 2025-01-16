42ee7543effdc91b7eb8ce87f88b5d55320e95f7
Cippe420 <buratti.1863851@studenti.uniroma1.it>
2025-01-16 11:00:21 +0000

    .

--- powerBenchmark.sh
+++ powerBenchmark.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 main ()
 {
-	#python3 currentMeasure.py &
 	touch cpuPower.txt
-	for i in {20..80..10}; do
+	echo 20 > cpuPower.txt
+	python3 currentMeasure.py &
+	touch cpuPower.txt
+	for i in {20..40..10}; do
 		echo $i > cpuPower.txt
 		stress-ng -c 0 -l $i -t 2s
 	done
