bc93615ea212d2d3d22ed1f3ca4b7af2aedfdda0
Cippe420 <buratti.1863851@studenti.uniroma1.it>
2025-01-16 12:02:42 +0100

    .

--- currentMeasure.py
+++ currentMeasure.py
@@ -5,23 +5,23 @@ def main():
 
     measurements={}
 
-    # with open('currentMeasure.csv','w') as f:
-    #     f.write('')
-    # while True:
-    #     # Open serial port and read data
-    #     ser = serial.Serial('/dev/ttyUSB0', 9600)
-    #     data = ser.readline().decode('utf-8').rstrip()
+    with open('currentMeasure.csv','w') as f:
+        f.write('')
+    while True:
+        # Open serial port and read data
+        ser = serial.Serial('/dev/ttyUSB0', 9600)
+        data = ser.readline().decode('utf-8').rstrip()
 
-    #     datalist = data.split()
+        datalist = data.split()
 
-    #     with open('cpuPower.txt','r') as f:
-    #         istante = f.read().strip()
-    #     if istante == 'stop':
-    #         break
-    #     else:   
-    #         # Print data
-    #         with open('currentMeasure.csv', 'a') as file:
-    #             file.write(istante + ', ' + datalist[1] +','+ datalist[3][1::] + '\n')
+        with open('cpuPower.txt','r') as f:
+            istante = f.read().strip()
+        if istante == 'stop':
+            break
+        else:   
+            # Print data
+            with open('currentMeasure.csv', 'a') as file:
+                file.write(istante + ', ' + datalist[1] +','+ datalist[3][1::] + '\n')
     #             if istante not in measurements:
     #                 measurements[istante] = (float(datalist[1]),float(datalist[3][1::]),1)
     #             else:
