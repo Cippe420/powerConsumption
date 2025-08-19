a507308a0495e4ec50d53fd5e8620f2e3568f84b
Cippe420 <buratti.1863851@studenti.uniroma1.it>
2025-08-19 14:30:02 +0100

    .

--- lpMode.sh
+++ lpMode.sh
@@ -6,18 +6,4 @@ sudo vcgencmd display_power 0
 
 echo "disabilito interfaccia ethernet..."
 sudo ifconfig eth0 down
-
-echo "cambio il governor in powersave..."
-echo "powersave" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-
-echo "Underclocking la CPU a 600 MHz..."
-echo 600000 | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
-echo 600000 | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq
-
-echo "Going to sleep, bye bye u.u"
-
 sudo ifconfig wlan0 down
-echo "i will sleep for 1h"
-sleep 1h
-sudo ifconfig wlan0 up
-echo "Done!"
---
+++ tmpScript.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+SECONDS=10
+BOTH=$(mktemp)
+CURRENT=$(mktemp)
+TENSION=$(mktemp)
+CONSUMPTION=$(mktemp)
+for i in $(seq 1 ${SECONDS})
+do
+	sleep 1
+	vcgencmd pmic_read_adc > ${BOTH}
+	cat ${BOTH} | grep current | awk '{print substr($2, 1, length($2)-1)}' | sed 's/.*=//g'            > ${CURRENT}
+	cat ${BOTH} | grep volt    | awk '{print substr($2, 1, length($2)-1)}' | sed 's/.*=//g' | head -12 > ${TENSION}
+	paste ${CURRENT} ${TENSION}| awk '{sum+=$1*$2}END{print sum}' >> ${CONSUMPTION}
+	rm ${CURRENT} ${TENSION} ${BOTH}
+done
+cat ${CONSUMPTION} | awk '{sumX+=+$1;sumX2+=(($1)^2)}END{if (NR>1) printf "Average_power_consumption= %.3f +/- %.3f W\n", sumX/(NR)*1.1451+0.5879, sqrt((sumX2-sumX^2/NR)/(NR-1)/NR)*1.1451}'
+rm ${CONSUMPTION}
